66f672ff44e7fcd1e1894554e3abf9e54f581c2c
Joseph Parker <2558907p@student.gla.ac.uk>
2022-01-28 15:34:50 +0000

    Chapter 4 Test Pass

--- rango/views.py
+++ rango/views.py
@@ -1,28 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
-		#Chapter 4
+	#Chapter 4
 
-	#def index(request):
-	#	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
-	#		# Construct a dictionary to pass to the template engine as its context.
-#
-#		return render(request, 'rango/index.html', context=context_dict)
-			# Return a rendered response to send to the client.
+def index(request):
+	context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+		# Construct a dictionary to pass to the template engine as its context.
 
+	return render(request, 'rango/index.html', context=context_dict)
+		# Return a rendered response to send to the client.
 
-#	def about(request):
-#		context_dict = {'boldmessage': 'This tutorial has been put together by Joseph Parker'}
-			# Construct a dictionary to pass to the template engine as its context.
+def about(request):
+	context_dict = {'boldmessage': 'This tutorial has been put together by Joseph Parker'}
+		# Construct a dictionary to pass to the template engine as its context.
 
-#		return render(request, 'rango/about.html', context=context_dict)
-			# Return a rendered response to send to the client.
+	return render(request, 'rango/about.html', context=context_dict)
+		# Return a rendered response to send to the client.
 
 	#Chapter 3
 
-def index(request): 
-	return HttpResponse("Rango says hey there partner! " + "<a href='/rango/about'>About</a>")
+#def index(request): 
+#	return HttpResponse("Rango says hey there partner! " + "<a href='/rango/about'>About</a>")
 
-def about(request):
-	return HttpResponse("Rango says here is the about page. " + "<a href='/rango/'>Index</a>")
+#def about(request):
+#	return HttpResponse("Rango says here is the about page. " + "<a href='/rango/'>Index</a>")
 
